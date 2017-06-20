2ecd8b3305a9ef59857f0cc6b6e330275db3034d
Zhou-Yu-Jin <1119235833@qq.com>
2017-06-20 15:23:34 +0800

    Update 爬取html天气页面.py

--- find_the_weather/爬取html天气页面.py
+++ find_the_weather/爬取html天气页面.py
@@ -9,15 +9,20 @@ def get_the_weather(city_name):
         if city_name in lines:
           city_data=line.split('=')[0].strip()
    #各个城市天气状况的Api
-  url=
+   url = ('https://api.seniverse.com/v3/weather/now.json?key=hfkphvdhmqghnrfq&location=beijing&language=zh-Hans&unit=c'.format(location=city_code))
+
+        response = urllib.request.urlopen(url)
+        weather_html = response.read()
+        json_data = json.loads(weather_html)
+        #print(json_data)
+        
+        
+        
+        return[weather]
+      
+      
+      except NameError :
+        print('不存在此城市或暂无数据')
     
   
 
->>> r=requests.get("http://m.weather.com.cn/mweather/101280101.shtml")
->>> r.status_code
-200
->>> r.encoding
-'ISO-8859-1'
->>> r.text[:1000]
-'ï»¿<!DOCTYPE HTML>\r\n<html>\r\n<head>\r\n<meta charset="utf-8">\r\n<meta name="viewport" content="width=device-width,initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">\r\n<link type="text/css" rel="stylesheet" href="http://i.tq121.com.cn/c/wap/wap.css?id=2016">\r\n<script type="text/javascript" src="http://i.tq121.com.cn/j/jquery-1.8.2.js?20140529_105543"></script>\r\n<style>\r\n.weather-two {\r\n\tposition:relative;\r\n\tcolor:#fff;\r\n\theight:108px;\r\n\tmargin: 20px auto 5px;\r\n\twidth:95%;\r\n\toverflow:hidden;\r\n\tmargin-bottom:5px;\r\n\t}\r\n.weather-two .weather-weather {\r\n\r\n}\r\n.weather-two .weather-weather i.svnicon {\r\n\r\n}\r\n.weather-wd {\r\n\tfont-size:100px;\r\n\tfloat: left;\r\n\theight:100px;\r\n\t\r\n\tmargin-left:10px;\r\n\t\r\n}\r\n.weather-wd-top {\r\n\tfloat:left;\r\n\tfont-size: 20px;\r\n    margin-top: 17px;\t\r\n}\r\n.weather-tqqk {\r\n\tfloat:left;\r\n\tmargin-top:41px;\r\n\r\n}\r\n.weather-tq {\r\nmargin-bottom: 12px;\r\n}\r\n.weather-gdw {\r\n}\r\n.weather-gdw i.iconfont {\r\n}\r\n.weather-three {\r\n\tclear:both;\r\n\twidth:95%;\r\n\tmargin:0 a'
->>> 
