368b63a827ec79bf1d58969a7a941ef5aa719b94
sunsipan <1635707508@qq.com>
2017-06-20 23:26:58 +0800

    Update find_the_weather_4web.py

--- find_the_weather/find_the_weather_4web.py
+++ find_the_weather/find_the_weather_4web.py
@@ -19,15 +19,7 @@ def entry_page() -> 'html':
     return render_template('entry.html',
                            the_title='欢迎来到网上天气查询！')
 
-@app.route('/pick_a_date', methods=['POST'])
-def pick_a_date() -> 'html':
-    """提取用户web 请求POST方法提交的数据（输入），不执行任何动作（处理），直接返回（输出）。"""
-    user_date = request.form['user_date']	
-    return render_template('results.html',
-                           the_title = '以下是您选取的日期：',
-                           the_date = user_date,
-                           the_results=results
-                           )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
