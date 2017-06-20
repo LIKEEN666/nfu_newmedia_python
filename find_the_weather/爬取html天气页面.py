f4a5e76fcb3b1c0e9c8bdba9e2883b1be795c821
sunsipan <1635707508@qq.com>
2017-06-20 13:34:40 +0800

    Update 爬取html天气页面.py

--- find_the_weather/爬取html天气页面.py
+++ find_the_weather/爬取html天气页面.py
@@ -1,6 +1,18 @@
 Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 18:41:36) [MSC v.1900 64 bit (AMD64)] on win32
 Type "copyright", "credits" or "license()" for more information.
->>> import requests
+import urllib.requests,json
+def get_the_weather(city_name):
+  try:
+    with open('各城市代码（尝试除广州外的其他城市天气）.txt',encoding='utf-8')as g:
+      lines=g.readlines()
+      for line in lines:
+        if city_name in lines:
+          city_data=line.split('=')[0].strip()
+   #各个城市天气状况的Api
+  url=
+    
+  
+
 >>> r=requests.get("http://m.weather.com.cn/mweather/101280101.shtml")
 >>> r.status_code
 200
