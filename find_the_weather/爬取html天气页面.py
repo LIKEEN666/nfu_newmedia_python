2a6ebd20073b13dda28f9d26a8d72cfa4a241a45
sunsipan <1635707508@qq.com>
2017-06-20 18:04:53 +0800

    Update 爬取html天气页面.py

--- find_the_weather/爬取html天气页面.py
+++ find_the_weather/爬取html天气页面.py
@@ -1,5 +1,3 @@
-Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 18:41:36) [MSC v.1900 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
 import urllib.requests,json
 def get_the_weather(city_name):
   try:
@@ -9,20 +7,17 @@ def get_the_weather(city_name):
         if city_name in lines:
           city_data=line.split('=')[0].strip()
    #各个城市天气状况的Api
-   url = ('https://api.seniverse.com/v3/weather/now.json?key=hfkphvdhmqghnrfq&location=guangzhou&language=zh-Hans&unit=c'.format(location=city_name))
+url = ('https://api.seniverse.com/v3/weather/now.json?key=hfkphvdhmqghnrfq&location=guangzhou&language=zh-Hans&unit=c'.format(location=city_name))
+response = urllib.request.urlopen(url)
 
-        response = urllib.request.urlopen(url)
-        weather_html = response.read()
-        json_data = json.loads(weather_html)
+weather_html = response.read()
+
+json_data = json.loads(weather_html)
+
         #print(json_data)
-        
-        
-        
-        return[weather]
-      
-      
-      except NameError :
-        print('不存在此城市或暂无数据')
+return[weather]
+except NameError :
+  print('不存在此城市或暂无数据')
     
   
 
