fd717b4f3a8e86ee70a6544c34a47c4f79787af6
Ye-Wen-Min <474253953@qq.com>
2017-06-30 11:40:52 +0800

    Update find_the_weather_4web.py

--- find_the_weather/find_the_weather_4web.py
+++ find_the_weather/find_the_weather_4web.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 @app.route('/pick_a_date', methods=['POST'])
 def pick_a_date() -> 'html':
-    date = request.form['date']
-    city = request.form['city']
-    results = find_the_weather
+    date = request.form['date'] #输入日期
+    city = request.form['city'] #输入城市名称
+    results = find_the_weather #输出有关日期和城市的相关天气
     return render_template('results.html',
                            the_title = '以下是您所查询的天气：',
                            the_date=date,
