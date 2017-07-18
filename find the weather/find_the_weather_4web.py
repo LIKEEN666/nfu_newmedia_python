1628a4c51da2769d2358ee20a028bb6703266e63
Li-Hong-Ni <1193097066@qq.com>
2017-07-18 22:34:50 +0800

    Update find_the_weather_4web.py

--- find the weather/find_the_weather_4web.py
+++ find the weather/find_the_weather_4web.py
@@ -14,7 +14,7 @@ def entry_page() -> 'html':#抓取html资料
 def pick_a_date() -> 'html':
     """提取用户web 请求POST方法提交的数据（输入），不执行任何动作（处理），直接返回（输出）。"""
     user_date = request.form['user_date']	#请求request模块
-    return render_template('results.html',
+    return render_template('results.html',#render_template的功能是先引入results.html，同时根据后面传入的参数，对html进行修改渲染。
                            the_title = '以下是您选取的日期：',
                            the_date = user_date,
                            )
