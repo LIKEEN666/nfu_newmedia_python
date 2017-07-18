b9d2fb203ded97e906f6af1b8f2cfd6276a96448
Ye-Wen-Min <474253953@qq.com>
2017-07-19 00:12:49 +0800

    Update main.py

--- find_the_weather/main.py
+++ find_the_weather/main.py
@@ -1,4 +1,4 @@
-import requests,json,urllib.request
+import requests,json,urllib.request #python中的import语句是用来导入模块的,在这里导入了requests和json模板
 def find_the_weather(city_name):
    #各个城市天气状况的Api
         url_api = ( 'https://api.seniverse.com/v3/weather/now.json?key=hfkphvdhmqghnrfq&location=%s&language=zh-Hans&unit=c'%city_name)
