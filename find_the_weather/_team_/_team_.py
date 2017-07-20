299efbc7d4f5db369074719c06a77a2ac8d7d7cf
Chen-Jia-lin <1213447537@qq.com>
2017-07-20 16:21:41 +0800

    Update _team_.py

--- find_the_weather/_team_/_team_.py
+++ find_the_weather/_team_/_team_.py
@@ -3,5 +3,5 @@ import csv #python中的import语句是用来导入模块的,在这里导入了C
 with open('_team_.tsv', encoding='utf8') as f: #对于大文件，用with open(xx) as fp:  for line in fp:方式来读取 
     reader = csv.DictReader(f) #用reader的方式对csv文件进行读取
     print ('\t'.join(reader.fieldnames))
-    for r in reader: #运用了for循环
+    for r in reader: 
         print ('\t'.join([r[k] for k in reader.fieldnames]) ) 
